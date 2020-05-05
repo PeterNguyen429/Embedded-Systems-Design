df951be99c69ad5b56f5cd487b8a194d274b82e7
Peter Nguyen <PeterNguyen429@gmail.com>
2020-05-05 04:33:53 -0400

    PDF and DOC of Labs

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include "mbed.h"
+
+AnalogOut wave(p18);
+
+int main() {
+    const double pi = 3.141592653589793238462;
+    const double amplitude = 0.5f;
+    const double offset = 65535/2;
+    double rads = 0.0;
+    uint16_t sample = 0;
+
+    while(1) {
+        // sinewave output
+        for (int i = 0; i < 20; i++) {
+            rads = (pi * i*18.0) / 180.0f;
+            sample = (uint16_t)(amplitude * (offset * (cos(rads + pi))) + offset);
+            wave.write_u16(sample);
+        }
+    }
+}
